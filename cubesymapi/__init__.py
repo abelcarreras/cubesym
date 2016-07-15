ee1511525da4d7f337083ecd553fbdde1a114164
abel <abelcarreras83@gmail.com>
2016-07-15 14:28:15 +0900

    Updated automatic bounds for Cartesian coordinates

--- cubesymapi/__init__.py
+++ cubesymapi/__init__.py
@@ -96,7 +96,7 @@ class Calculation:
 
         distances = [np.linalg.norm(np.cross(vector, center-np.array(point)))/np.linalg.norm(vector) for point in corner]
         positions = [-np.dot(center-np.array(point), vector)/np.linalg.norm(vector) for point in corner]
-        max_rad = np.max(distances)
+        max_rad = np.min(distances)
 
         print ('Oriented cube bounds')
         if radial:
